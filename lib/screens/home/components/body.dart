03d7dfc6039ca53424058b80fa76f5ca84deb261
Carlos Doki <carlosdoki@gmail.com>
2020-07-21 11:01:24 -0300

    Profile

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:recipe_app/models/RecipeBundle.dart';
-
 import 'package:recipe_app/screens/home/components/categories.dart';
 import 'package:recipe_app/screens/home/components/recipe_bundle_card.dart';
 import 'package:recipe_app/size_config.dart';
--- lib/screens/profile/components/body.dart
+++ lib/screens/profile/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:recipe_app/constants.dart';
+import 'package:recipe_app/screens/profile/components/info.dart';
+import 'package:recipe_app/screens/profile/components/profile_menu_item.dart';
 import 'package:recipe_app/size_config.dart';
 
 class Body extends StatelessWidget {
@@ -7,58 +8,39 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     double defaultSize = SizeConfig.defaultSize;
 
-    return Column(
-      children: <Widget>[
-        SizedBox(
-          height: defaultSize * 24,
-          child: Stack(
-            children: <Widget>[
-              ClipPath(
-                clipper: CustomShape(),
-                child: Container(
-                  height: defaultSize * 15,
-                  color: kPrimaryColor,
-                ),
-              ),
-              Column(
-                children: <Widget>[
-                  Container(
-                    height: defaultSize * 14,
-                    width: defaultSize * 14,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: AssetImage(
-                          "assets/images/pic.png",
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Info(
+            image: "assets/images/pic.png",
+            name: "Jhon Doe",
+            email: "Jhondoe@gmail.com",
           ),
-        )
-      ],
+          SizedBox(
+            height: SizeConfig.defaultSize * 2,
+          ),
+          ProfileMenuItem(
+            iconSrc: "assets/icons/bookmark_fill.svg",
+            title: "Save Recipes",
+            press: () {},
+          ),
+          ProfileMenuItem(
+            iconSrc: "assets/icons/chef_color.svg",
+            title: "Super Plan",
+            press: () {},
+          ),
+          ProfileMenuItem(
+            iconSrc: "assets/icons/language.svg",
+            title: "Change Language",
+            press: () {},
+          ),
+          ProfileMenuItem(
+            iconSrc: "assets/icons/info.svg",
+            title: "Help",
+            press: () {},
+          ),
+        ],
+      ),
     );
-  }
-}
-
-class CustomShape extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    double height = size.height;
-    double width = size.width;
-    path.lineTo(0, height - 100);
-    path.quadraticBezierTo(width / 2, height, width, height - 100);
-    path.lineTo(width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return true;
   }
 }
